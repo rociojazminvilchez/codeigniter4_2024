03a9401e3f53f898d6270ca6790e1aa20dada7cb
arte-estudio <rociojazminvilchez@gmail.com>
2024-04-28 17:59:42 -0300

    a

---
+++ app/Controllers/Noticias.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controllers;
+
+class Noticias extends BaseController{
+    public function index(){
+        echo ("hola"); 
+    }
+
+    public function show(){
+        return "nueva funcion";
+    }
+}
+?>
